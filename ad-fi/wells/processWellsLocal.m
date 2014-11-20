9873c85ea31a1fef54d271d8e5b0e0e63aa3b818
Xavier Raynaud <xavier.raynaud@sintef.no>
2014-11-20 08:14:08 +0100

    Fixed depth reordering.
    Added reordering of cell status.

--- ad-fi/wells/processWellsLocal.m
+++ ad-fi/wells/processWellsLocal.m
@@ -600,12 +600,14 @@ function W = reorderByDepth(G, W)
          cells = W(i).cells;
          WI = W(i).WI;
          dZ = W(i).dZ;
+         cstatus = W(i).cstatus;
          % depth = G.cells.centroids(cells, 3);
          index = (1:numel(cells))';
-         new_index = sortrows(horzcat(dZ, index, cells, WI));
+         new_index = sortrows(horzcat(dZ, index, cells, WI, cstatus));
          W(i).dZ = new_index(:, 1);
          W(i).cells = new_index(:, 3);
          W(i).WI = new_index(:, 4);
+         W(i).cstatus = new_index(:, 5);
       end
    end
 end
