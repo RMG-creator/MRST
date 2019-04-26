a02b4f6158b80494f311261993b6939e5486ccb0
Xavier Raynaud <xav.raynaud@gmail.com>
2019-04-26 19:07:07 +0200

    started with component approach

---
+++ ad-eor/models/GenericBlackOilSurfactantModel.m
@@ -0,0 +1,82 @@
+classdef GenericBlackOilSurfactantModel < GenericBlackOilModel
+    properties
+        surfactant
+    end
+
+    methods
+        function model = GenericBlackOilSurfactantModel(G, rock, fluid, varargin)
+            model = model@GenericBlackOilModel(G, rock, fluid, varargin{:});
+            model.FlowPropertyFunctions = ...
+                SurfactantFlowPropertyFunctions(model);
+            nph = model.getNumberOfPhases();
+            model.Components = cell(1, nph);
+            names = model.getPhaseNames();
+            disgas = model.disgas;
+            vapoil = model.vapoil;
+            for ph = 1 : nph
+                switch names(ph)
+                  case 'W'
+                    c = ImmiscibleComponent('water', ph);
+                  case 'O'
+                    if disgas || vapoil
+                        c = OilComponent('oil', ph, disgas, vapoil);
+                    else
+                        c = ImmiscibleComponent('oil', ph);
+                    end
+                  case 'G'
+                    if disgas || vapoil
+                        c = GasComponent('gas', ph, disgas, vapoil);
+                    else
+                        c = ImmiscibleComponent('gas', ph);
+                    end
+                  otherwise
+                    error('Unknown phase');
+                end
+                model.Components{ph} = c;
+            end
+            model.Components{end + 1} = ImmiscibleComponent('surfactant', 'W');
+        end
+
+        function [problem, state] = getEquations(model, state0, state, dt, drivingForces, varargin)
+            [problem, state] = getEquations@PhysicalModel(model, state0, state, dt, drivingForces, varargin{:});
+        end
+
+        function [eqs, names, types, state] = getModelEquations(model, state0, state, dt, drivingForces)
+            [eqs, flux, names, types] = model.FluxDiscretization.componentConservationEquations(model, state, state0, dt);
+            src = model.FacilityModel.getComponentSources(state);
+            % Assemble equations and add in sources
+            for i = 1:numel(eqs)
+                if ~isempty(src.cells)
+                    eqs{i}(src.cells) = eqs{i}(src.cells) - src.value{i};
+                end
+                eqs{i} = model.operators.AccDiv(eqs{i}, flux{i});
+            end
+            % Get facility equations
+            [weqs, wnames, wtypes, state] = model.FacilityModel.getModelEquations(state0, state, dt, drivingForces);
+            eqs = [eqs, weqs];
+            names = [names, wnames];
+            types = [types, wtypes];
+        end
+
+        function names = getComponentNames(model)
+            names = cellfun(@(x) x.name, model.Components, 'UniformOutput', false);
+        end
+
+        function [state, report] = updateState(model, state, problem, dx, forces)
+            [state, report] = updateState@ThreePhaseBlackOilModel(model, state, problem, dx, forces);
+            if ~isempty(model.FacilityModel)
+                state = model.FacilityModel.applyWellLimits(state);
+            end
+        end
+
+        function [state, report] = updateAfterConvergence(model, state0, state, dt, drivingForces)
+            [state, report] = updateAfterConvergence@ReservoirModel(model, state0, state, dt, drivingForces);
+            if model.outputFluxes
+                f = model.getProp(state, 'PhaseFlux');
+                nph = numel(f);
+                state.flux = zeros(model.G.faces.num, nph);
+                state.flux(model.operators.internalConn, :) = [f{:}];
+            end
+        end
+    end
+end
