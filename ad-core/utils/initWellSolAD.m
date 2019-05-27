578cf7cb145d9ad35e469b434f46ddbf4b62664a
pesunxin <B16020075@s.upc.edu.cn>
2019-05-27 16:28:35 +0800

    change schedule.control.c to  schedule.control.cp and  schedule.control.cs

--- ad-core/utils/initWellSolAD.m
+++ ad-core/utils/initWellSolAD.m
@@ -127,15 +127,14 @@ for k = 1:nw
     if isprop(model, 'solvent') && model.solvent
        ws(k).qSs = W(k).sign*irate;
     end
-    cnames = model.getComponentNames();
-    for i = 1:numel(cnames)
-        switch lower(cnames{i})
-            case {'polymer', 'surfactant'};
-                % Water based EOR
-                ws(k).c(i) = W(k).c(i)*ws(k).qWs;
-            otherwise
-                % No good guess.
-        end
+
+    if model.polymer && model.surfactant
+        ws(k).cp = W(k).cp*ws(k).qWs;
+        ws(k).cs = W(k).cs*ws(k).qWs;
+    elseif model.polymer && ~model.surfactant
+        ws(k).cp = W(k).cp*ws(k).qWs;
+    elseif model.surfactant
+        ws(k).cs = W(k).cs*ws(k).qWs;
     end
 
     ws(k).mixs = W(k).compi;
@@ -176,15 +175,13 @@ for k = 1:numel(W)
                 ix = 1 + model.water + model.oil + model.gas;
                 ws(k).qSs = v*W(k).compi(ix);
             end
-            cnames = model.getComponentNames();
-            for i = 1:numel(cnames)
-                switch lower(cnames{i})
-                    case {'polymer', 'surfactant'};
-                        % Water based EOR
-                        ws(k).c(i) = W(k).c(i)*ws(k).qWs;
-                    otherwise
-                        % No good guess.
-                end
+            if model.polymer && model.surfactant
+               ws(k).cp = W(k).cp*ws(k).qWs;
+               ws(k).cs = W(k).cs*ws(k).qWs;
+            elseif model.polymer && ~model.surfactant
+               ws(k).cp = W(k).cp*ws(k).qWs;
+            elseif model.surfactant
+               ws(k).cs = W(k).cs*ws(k).qWs;
             end
         case 'orat'
             ws(k).qOs = v;
