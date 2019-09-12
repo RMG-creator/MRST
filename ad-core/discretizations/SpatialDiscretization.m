e8bb0279b73d9e9691f9acbbbd118388866d6b57
Øystein S. Klemetsdal <oystein.klemetsdal@gmail.com>
2019-09-12 17:14:45 +0200

    Added operators to SpatialDiscretization

--- ad-core/discretizations/SpatialDiscretization.m
+++ ad-core/discretizations/SpatialDiscretization.m
@@ -15,6 +15,7 @@ classdef SpatialDiscretization
             N                 = G.faces.neighbors;
             disc.internalConn = all(N ~= 0, 2);
             disc.N            = N(disc.internalConn,:);
+            disc.operators    = model.operators;
         end
         
     end
